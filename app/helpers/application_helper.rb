fa5556d3833b46b785d594c2d2c04d59d42cb42d
Karlysson Alves <karlysson.alves@gmail.com>
2020-03-09 23:20:17 -0300

    Action Cable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     File.open("app/assets/images/#{path}", "rb") { |file| raw(file.read) }  
   end
   
+  def user_avatar(user)
+    
+    if user.avatar.attached?
+      avatar = user.avatar.variant(combine_options: { resize: '100x100^', extent: '100x100' })
+    else
+      avatar = "default-avatar.jpg"
+    end
+
+    image_tag avatar, class: "photo"
+  end
+  
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -18,3 +20,4 @@ require("channels")
 import "../src/application.css";
 import "./flashMessages";
 import "./modal";
+import "./likes";
