8d959a492682c8c743238d72fedd91a4c01b5b92
Karlysson Alves <karlysson.alves@gmail.com>
2020-03-09 23:16:47 -0300

    Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,17 @@
 class Post < ApplicationRecord
 
   belongs_to :created_by, class_name: "User"
-
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  
+  
+  has_one_attached :photo
+  validates :photo, attached: true, content_type: %i[png jpg jpeg], size: { less_than: 5.megabytes }
+  
   validates_presence_of :description
+
+  def liked_by?(user)
+    likes.where(user: user).exists?
+  end
+  
 end
